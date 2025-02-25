f798219486ea3260eaf1c7b1a6a450393c80ca58
Peter O'Leary <pete@mudflapinc.com>
2025-02-24 18:29:36 -0800

    Add pds server instead

--- main.ts
+++ main.ts
@@ -1,32 +1,32 @@
 /* eslint-env node */
 
-// Tracer code above must come before anything else
-const path = require('node:path')
-const assert = require('node:assert')
-const cluster = require('cluster')
-const { Secp256k1Keypair } = require('@atproto/crypto')
-const { ServerConfig, BskyAppView } = require('@atproto/bsky')
+'use strict'
+
+const {
+  PDS,
+  envToCfg,
+  envToSecrets,
+  readEnv,
+  httpLogger,
+} = require('@atproto/pds')
+const pkg = require('@atproto/pds/package.json')
 
 const main = async () => {
-  const env = getEnv()
-  const config = ServerConfig.readEnv()
-  assert(env.serviceSigningKey, 'must set BSKY_SERVICE_SIGNING_KEY')
-  const signingKey = await Secp256k1Keypair.import(env.serviceSigningKey)
-  const bsky = BskyAppView.create({ config, signingKey })
-  await bsky.start()
+  const env = readEnv()
+  env.version ??= pkg.version
+  const cfg = envToCfg(env)
+  const secrets = envToSecrets(env)
+  const pds = await PDS.create(cfg, secrets)
+
+  await pds.start()
+
+  httpLogger.info('pds is running')
   // Graceful shutdown (see also https://aws.amazon.com/blogs/containers/graceful-shutdowns-with-ecs/)
-  const shutdown = async () => {
-    await bsky.destroy()
-  }
-  process.on('SIGTERM', shutdown)
-  process.on('disconnect', shutdown) // when clustering
+  process.on('SIGTERM', async () => {
+    httpLogger.info('pds is stopping')
+    await pds.destroy()
+    httpLogger.info('pds is stopped')
+  })
 }
 
-const getEnv = () => ({
-  serviceSigningKey: process.env.BSKY_SERVICE_SIGNING_KEY || undefined,
-})
-
-main().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+main()
